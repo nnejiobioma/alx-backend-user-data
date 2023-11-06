ae83bfee71573defced844ec40133151ac295e4f
Nneji Obioma <nnejiobioma@gmail.com>
2023-11-06 20:37:05 +0100

    task handled

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,18 +7,14 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
+    """ GET /api/v1/status. The status of the API
     """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
+    """ GET /api/v1/stats. The number of each objects
     """
     from models.user import User
     stats = {}
@@ -28,19 +24,13 @@ def stats() -> str:
 
 @app_views.route('/unauthorized/', strict_slashes=False, methods=['GET'])
 def unauthorized_endpoint() -> None:
-    """Endpoint that raises a 401 error.
-
-    Returns:
-        None: None.
+    """That raises a 401 error.
     """
     abort(401)
 
 
 @app_views.route('/forbidden/', strict_slashes=False, methods=['GET'])
 def forbidden_endpoint() -> None:
-    """Endpoint that raises a 403 error.
-
-    Returns:
-        None: None.
+    """That raises a 403 error.
     """
     abort(403)
